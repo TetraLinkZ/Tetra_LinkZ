603a8c63fff4a20950f3c84f675903c749dffa8d
Rafael Tongson <rtongson@peppymiint.local>
2019-09-23 13:44:01 -0700

    initial commit

---
+++ src/main/java/com/tetralinkz/tetralinkz/TetraLinkZApplication.java
@@ -0,0 +1,13 @@
+package com.tetralinkz.tetralinkz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetraLinkZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetraLinkZApplication.class, args);
+	}
+
+}
