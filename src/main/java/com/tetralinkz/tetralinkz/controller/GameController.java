3da8c075b0266f9aec111c6428fcd276404fb81a
Zhen Yang <junkmai92@gmail.com>
2019-09-23 16:39:44 -0700

    fix game service and modified game controller

--- src/main/java/com/tetralinkz/tetralinkz/controller/GameController.java
+++ src/main/java/com/tetralinkz/tetralinkz/controller/GameController.java
@@ -2,7 +2,15 @@ package com.tetralinkz.tetralinkz.controller;
 
 import org.springframework.stereotype.Controller;
 
+import com.tetralinkz.tetralinkz.services.GameService;
+
 @Controller
 public class GameController {
-
+	private final GameService gameService;
+	private final MainService mainService;
+	
+	public GameController(GameService gameService, MainService mainService) {
+		this.gameService = gameService;
+		this.mainService = mainService;
+	}
 }
--- src/main/java/com/tetralinkz/tetralinkz/services/GameService.java
+++ src/main/java/com/tetralinkz/tetralinkz/services/GameService.java
@@ -2,8 +2,8 @@ package com.tetralinkz.tetralinkz.services;
 
 import org.springframework.stereotype.Service;
 
-import com.tetralinkz.tetralinkz.models.MatchHistory;
 import com.tetralinkz.tetralinkz.repositories.AvatarRepository;
+import com.tetralinkz.tetralinkz.repositories.MatchHistoryRepository;
 import com.tetralinkz.tetralinkz.repositories.TokenRepository;
 import com.tetralinkz.tetralinkz.repositories.UserRepository;
 
@@ -12,10 +12,10 @@ public class GameService {
 	private final UserRepository userRepo;
 	private final TokenRepository tokenRepo;
 	private final AvatarRepository avatarRepo;
-	private final MatchHistory historyRepo;
+	private final MatchHistoryRepository historyRepo;
 	
 	public GameService(UserRepository userRepo, TokenRepository tokenRepo, AvatarRepository avatarRepo,
-			MatchHistory historyRepo) {
+			MatchHistoryRepository historyRepo) {
 		this.userRepo = userRepo;
 		this.tokenRepo = tokenRepo;
 		this.avatarRepo = avatarRepo;
