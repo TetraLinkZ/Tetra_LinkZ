8c2bfaae0b0b5e97a47659bd947fe06a06653763
Rafael Tongson <rtongson@peppymiint.local>
2019-09-25 16:47:44 -0700

    make rest api to update match

--- src/main/java/com/tetralinkz/tetralinkz/controller/GameApi.java
+++ src/main/java/com/tetralinkz/tetralinkz/controller/GameApi.java
@@ -2,6 +2,7 @@ package com.tetralinkz.tetralinkz.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tetralinkz.tetralinkz.models.Match;
@@ -37,6 +38,46 @@ public class GameApi {
 		mainService.updateUser(u1);
 		mainService.updateUser(u2);
 		return;
+	}
+	
+	@PostMapping("/game/play/move")
+	public String gameAction(
+			Match match, 
+			@RequestParam("row")int row, 
+			@RequestParam("column")int column,
+			@RequestParam("user") User user,
+			@RequestParam("player")int oneOrTwo,
+			@RequestParam("win") boolean win
+			) {
+		
+		//current player token is player one or two;
+		
+		// Retrieve the match's current board
+		String board = match.getBoard();
+		// Start board index at 0 for iteration
+		int strBoardIdx = 0;
+		// Instantiate matrix for board
+		int[][] matrix = new int[7][7];	
+		// iterate through the board string and add elements to matrix
+		for(int i = 0; i < 7; i++) {
+			for(int j = 0; j < 7; j++) {
+				matrix[i][j] = Character.getNumericValue(board.charAt(strBoardIdx));
+				strBoardIdx++;
+			}
+		}
+		
+		// place the player one or two token into the row and column;
+		matrix[row][column] =  oneOrTwo;
+		
+		StringBuilder stringMatrix = new StringBuilder();
+		for(int x = 0; x < 7; x++) {
+			for(int y = 0; y < 7; y++) {
+				stringMatrix.append(matrix[x][y]);
+			}
+		}
+		match.setBoard(stringMatrix.toString());
+		gameService.newMatch(match);
+		return stringMatrix.toString();
 	}
 
 }
