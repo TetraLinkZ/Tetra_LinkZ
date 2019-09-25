89d350a5a986986510566175913949086837e951
Zhen Yang <junkmai92@gmail.com>
2019-09-25 11:42:33 -0700

    item page can now display all infor of user owned items

--- src/main/java/com/tetralinkz/tetralinkz/repositories/UserAvatarRepository.java
+++ src/main/java/com/tetralinkz/tetralinkz/repositories/UserAvatarRepository.java
@@ -1,11 +1,14 @@
 package com.tetralinkz.tetralinkz.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.tetralinkz.tetralinkz.models.User;
 import com.tetralinkz.tetralinkz.models.UserAvatar;
 
 @Repository
 public interface UserAvatarRepository extends CrudRepository<UserAvatar, Long>{
-
+	List<UserAvatar> findByUser(User user);
 }
--- src/main/java/com/tetralinkz/tetralinkz/repositories/UserTokenRepository.java
+++ src/main/java/com/tetralinkz/tetralinkz/repositories/UserTokenRepository.java
@@ -1,11 +1,14 @@
 package com.tetralinkz.tetralinkz.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.tetralinkz.tetralinkz.models.User;
 import com.tetralinkz.tetralinkz.models.UserToken;
 
 @Repository
 public interface UserTokenRepository extends CrudRepository<UserToken, Long>{
-	
+	List<UserToken> findByUser(User user);
 }
