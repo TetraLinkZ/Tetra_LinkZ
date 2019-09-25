afd73ea69e1046258e8c7d5ad344332e0a438b8b
Dana Hedrick <dana.marie.hedrick@gmail.com>
2019-09-25 07:50:27 -0700

    test

--- src/main/java/com/tetralinkz/tetralinkz/services/MainService.java
+++ src/main/java/com/tetralinkz/tetralinkz/services/MainService.java
@@ -25,7 +25,7 @@ public class MainService {
 	private final MatchHistoryRepository historyRepo;
 	private final UserAvatarRepository uaRepo;
 	private final UserTokenRepository utRepo;
-		
+
 	public MainService(UserRepository userRepo, TokenRepository tokenRepo, AvatarRepository avatarRepo,
 			MatchHistoryRepository historyRepo, UserAvatarRepository uaRepo, UserTokenRepository utRepo) {
 		this.userRepo = userRepo;
@@ -37,101 +37,103 @@ public class MainService {
 	}
 
 	public User registerUser(User user) {
-        String hashed = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
-        user.setPassword(hashed);
-        return userRepo.save(user);
-    }
-    
-    // find user by email
-    public User findByEmail(String email) {
-        return userRepo.findByEmail(email);
-    }
-    
-    // find user by id
-    public User findUserById(Long id) {
-    	Optional<User> u = userRepo.findById(id);
-    	
-    	if(u.isPresent()) {
-            return u.get();
-    	} else {
-    	    return null;
-    	}
-    }
-    
-    // authenticate user
-    public boolean authenticateUser(String email, String password) {
-        // first find the user by email
-        User user = userRepo.findByEmail(email);
-        // if we can't find it by email, return false
-        if(user == null) {
-            return false;
-        } else {
-            // if the passwords match, return true, else, return false
-            if(BCrypt.checkpw(password, user.getPassword())) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-	
-    // // // // // // // //
-    // ADMIN CRUD SERVICES
-    
-    // INDEXING
-    
-    public List<Avatar> allAvatars(){
-    	return avatarRepo.findAll();
-    }
-    
-    public List<Token> allTokens() {
-    	return tokenRepo.findAll();
-    }
-    
-    //
-    
-    public Avatar getAvatarById(Long id) {
-    	Optional<Avatar> optAvatar = avatarRepo.findById(id);
-    	if(optAvatar.isPresent()) {
-    		return optAvatar.get();
-    	} else {
-    		return null;
-    	}
-    }
-    
-    public Token getTokenById(Long id) {
-    	Optional<Token> optToken = tokenRepo.findById(id);
-    	if(optToken.isPresent()) {
-    		return optToken.get();
-    	} else {
-    		return null;
-    	}
-    }
-    
-    // CREATE && UPDATE
-    public Avatar createOrUpdateAvatar(Avatar avatar) {
-    	return avatarRepo.save(avatar);
-    }
-    
-    public Token createOrUpdateToken(Token token) {
-    	return tokenRepo.save(token);
-    }
-    
-    // Establish Default Avatar
-    public void defaultAvatar(User user, Avatar avatar) {
-    	UserAvatar ua = new UserAvatar();
-    	ua.setUser(user);
-    	ua.setAvatar(avatar);
-    	user.setCurrentAvatar(avatar);
-    	uaRepo.save(ua);
-    }
-    
-    // Find Avatar by Id
-    public Avatar findAvatar(Long id) {
-    	Optional<Avatar> o = avatarRepo.findById(id);
-    	if(o.isPresent()) {
-    		return o.get();
-    	}
-    	return null;
-    }
+		String hashed = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+		user.setPassword(hashed);
+		return userRepo.save(user);
+	}
+
+	// find user by email
+	public User findByEmail(String email) {
+		return userRepo.findByEmail(email);
+	}
+
+	// find user by id
+	public User findUserById(Long id) {
+		Optional<User> u = userRepo.findById(id);
+
+		if (u.isPresent()) {
+			return u.get();
+		} else {
+			return null;
+		}
+	}
+
+	// authenticate user
+	public boolean authenticateUser(String email, String password) {
+		// first find the user by email
+		User user = userRepo.findByEmail(email);
+		// if we can't find it by email, return false
+		if (user == null) {
+			return false;
+		} else {
+			// if the passwords match, return true, else, return false
+			if (BCrypt.checkpw(password, user.getPassword())) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	// // // // // // // //
+	// ADMIN CRUD SERVICES
+
+	// INDEXING
+
+	public List<Avatar> allAvatars() {
+		return avatarRepo.findAll();
+	}
+
+	public List<Token> allTokens() {
+		return tokenRepo.findAll();
+	}
+
+	//
+
+	public Avatar getAvatarById(Long id) {
+		Optional<Avatar> optAvatar = avatarRepo.findById(id);
+		if (optAvatar.isPresent()) {
+			return optAvatar.get();
+		} else {
+			return null;
+		}
+	}
+
+	public Token getTokenById(Long id) {
+		Optional<Token> optToken = tokenRepo.findById(id);
+		if (optToken.isPresent()) {
+			return optToken.get();
+		} else {
+			return null;
+		}
+	}
+
+	// CREATE && UPDATE
+	public Avatar createOrUpdateAvatar(Avatar avatar) {
+		return avatarRepo.save(avatar);
+	}
+
+	public Token createOrUpdateToken(Token token) {
+		return tokenRepo.save(token);
+	}
+
+	// Establish Default Avatar
+	public void defaultAvatar(User user, Avatar avatar) {
+		UserAvatar ua = new UserAvatar();
+		ua.setUser(user);
+		ua.setAvatar(avatar);
+		user.setCurrentAvatar(avatar);
+		userRepo.save(user);
+		uaRepo.save(ua);
+
+	}
+
+	// Find Avatar by Id
+	public Avatar findAvatar(Long id) {
+		Optional<Avatar> o = avatarRepo.findById(id);
+		if (o.isPresent()) {
+			return o.get();
+		}
+		return null;
+	}
 }
