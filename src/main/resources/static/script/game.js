d3afbe2c0bcf5c6851957021ab4faa4494012b3c
Rafael Tongson <rtongson@peppymiint.local>
2019-09-30 12:03:37 -0700

    initial commit

--- src/main/resources/static/script/game.js
+++ src/main/resources/static/script/game.js
@@ -1,74 +1,143 @@
-(function() {
-    // Load the script
-    var script = document.createElement("SCRIPT");
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
-    script.type = 'text/javascript';
-    script.onload = function() {
-        var $ = window.jQuery;
-        const drawGame = () => {
-            let output = '';
-            
-            for (let row = 0; row < 7; row++) {
-              output += '<div class="row row'+row+' empty">';
-              for (col = 0; col < 7; col++) {
-                output += "<div class='column column" + col + " empty'></div>";
-              }
-              output += '</div>';
-            }
-            $("#game-board").append(output);
+//(function() {
+//    // Load the script
+//    var script = document.createElement("SCRIPT");
+//    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+//    script.type = 'text/javascript';
+//    script.onload = function() {
+//        var $ = window.jQuery;
+//        const drawGame = () => {
+//            let output = '';
+//            
+//            for (let row = 0; row < 7; row++) {
+//              output += '<div class="row row'+row+' empty">';
+//              for (col = 0; col < 7; col++) {
+//                output += "<div class='column column" + col + " empty'></div>";
+//              }
+//              output += '</div>';
+//            }
+//            $("#game-board").append(output);
+//          }
+//
+//          drawGame();
+//          console.log("hello, welcome to the game!");
+//
+//          // TEST CONTROLS
+//
+////          $("#test").click(function() {
+//    	  $(".column").click(function() {
+//            // ".column"
+//            let payload = {
+//              board_data: "0002000001102000222100012220002111000122100012110",
+//              player: 2,
+//              i: 3,
+//              j: 3
+//            }
+//
+//            $.ajax({
+//                url: "http://kevinalbs.com/connect4/back-end/index.php/hasWon",
+//                method: "GET",
+//                data: payload,
+//                success: function(data) {
+//                  console.log("player win with the move? :" + data);
+//                  payload.win = data;
+//                  $.ajax({
+//                    url: "/game/play/move",
+//                    method: "POST",
+//                    headers: {"X-CSRF-TOKEN": $("meta[name='_csrf']").attr("content")},
+//                    data: {
+//                      match: 2,
+//                      row: payload.i, 
+//                      column: payload.j,
+//                      user: 2,
+//                      player: payload.player,
+//                      // win: payload.win
+//                      win: false
+//                    },
+//                    success: function(data) {
+//                      console.log("current board: " + data);
+//                    },
+//                    dataType: "json"
+//                  });
+//                },
+//                dataType: "json"
+//            });
+//            
+//            $.ajax({
+//                url: "/game/play/move",
+//                method: "POST",
+//                headers: {"X-CSRF-TOKEN": $("meta[name='_csrf']").attr("content")},
+//                data: {
+//                  match: 2,
+//                  row: payload.i,
+//                  column: payload.j,
+//                  user: 2,
+//                  player: payload.player,
+//                  // win: payload.win
+//                  win: false
+//                },
+//                success: function(data) {
+//                  console.log("current board: " + data);
+//                },
+//                dataType: "json",
+//                success: (data)=>{
+//                	console.log("success: ", data)
+//                },
+//                error: (err)=>{
+//                	console.log("error log: ", err);
+//                }
+//              });
+//            console.log("hello from POST ajax")
+//            return false;
+//          })
+//    };
+//    
+////    END SCRIPT
+//    document.getElementsByTagName("head")[0].appendChild(script);
+//})();
+
+$(document).ready(function() {
+	const drawGame = () => {
+        let output = '';
+        
+        for (let row = 0; row < 7; row++) {
+          output += '<div class="row row'+row+' empty">';
+          for (col = 0; col < 7; col++) {
+            output += "<div class='column column" + col + " empty'></div>";
           }
+          output += '</div>';
+        }
+        $("#game-board").append(output);
+      }
 
-          drawGame();
-          console.log("hello, welcome to the game!");
+      drawGame();
+      console.log("hello, welcome to the game!");
 
-          // TEST CONTROLS
+      // TEST CONTROLS
 
-//          $("#test").click(function() {
-    	  $(".column").click(function() {
-            // ".column"
-            let payload = {
-              board_data: "0002000001102000222100012220002111000122100012110",
-              player: 2,
-              i: 3,
-              j: 3
-            }
+//      $("#test").click(function() {
+	  $(".column").click(function() {
+        // ".column"
+        let payload = {
+          board_data: "0002000001102000222100012220002111000122100012110",
+          player: 2,
+          i: 3,
+          j: 3
+        }
 
-            $.ajax({
-                url: "http://kevinalbs.com/connect4/back-end/index.php/hasWon",
-                method: "GET",
-                data: payload,
-                success: function(data) {
-                  console.log("player win with the move? :" + data);
-                  payload.win = data;
-                  $.ajax({
-                    url: "/game/play/move",
-                    method: "POST",
-                    headers: {"X-CSRF-TOKEN": $("meta[name='_csrf']").attr("content")},
-                    data: {
-                      match: 2,
-                      row: payload.i, 
-                      column: payload.j,
-                      user: 2,
-                      player: payload.player,
-                      // win: payload.win
-                      win: false
-                    },
-                    success: function(data) {
-                      console.log("current board: " + data);
-                    },
-                    dataType: "json"
-                  });
-                },
-                dataType: "json"
-            });
-            
-            $.ajax({
+        $.ajax({
+            url: "http://kevinalbs.com/connect4/back-end/index.php/hasWon",
+            method: "GET",
+            data: payload,
+            success: function(data) {
+              console.log("player win with the move? :" + data);
+              payload.win = data;
+              $.ajax({
                 url: "/game/play/move",
                 method: "POST",
                 headers: {"X-CSRF-TOKEN": $("meta[name='_csrf']").attr("content")},
                 data: {
                   match: 2,
-                  row: payload.i,
+                  row: payload.i, 
                   column: payload.j,
                   user: 2,
                   player: payload.player,
@@ -78,41 +147,40 @@
                 success: function(data) {
                   console.log("current board: " + data);
                 },
-                dataType: "json",
-                success: (data)=>{
-                	console.log("success: ", data)
-                },
-                error: (err)=>{
-                	console.log("error log: ", err);
-                }
+                dataType: "json"
               });
-            console.log("hello from POST ajax")
-            return false;
-          })
-    };
-    
-    $("#send").submit(function() {
-    	console.log("hello from #test ajax")
-    	
-    	var body = $(this).serialize();
-
-        $.ajax({
-			  url: "/api/credits",
-			  method: "POST",
-			  data: body,
-			  success: function(data) {
-          console.log(data);
-          console.log("clicked");
-			  },
-			  dataType: "json"
-		});
-		
-		return false;
+            },
+            dataType: "json"
+        });
         
-    })
-    
-    
-//    END SCRIPT
-    document.getElementsByTagName("head")[0].appendChild(script);
-})();
-
+        $.ajax({
+            url: "/game/play/move",
+            method: "POST",
+            headers: {"X-CSRF-TOKEN": $("meta[name='_csrf']").attr("content")},
+            data: {
+              match: 2,
+              row: payload.i,
+              column: payload.j,
+              user: 2,
+              player: payload.player,
+              // win: payload.win
+              win: false
+            },
+            success: function(data) {
+              console.log("current board: " + data);
+            },
+            dataType: "json",
+            success: (data)=>{
+            	console.log("success: ", data)
+            },
+            error: (err)=>{
+            	console.log("error log: ", err);
+            }
+          });
+        console.log("hello from POST ajax")
+        return false;
+      })
+};
+	
+	// END SCRIPT
+})
